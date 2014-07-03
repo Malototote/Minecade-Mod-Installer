f80536e7c15fe82bab5c920604fb87bcde155d67
Paul Sauve <paul@paulbgd.me>
2014-07-03 16:31:00 -0500

    Cleanup

--- src/main/java/com/minecade/mods/ModInstaller.java
+++ src/main/java/com/minecade/mods/ModInstaller.java
@@ -7,13 +7,10 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.util.Arrays;
-import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import org.apache.commons.io.FileUtils;
@@ -25,7 +22,7 @@ public class ModInstaller {
 
     private static final String OS = System.getProperty("os.name").toLowerCase();
     public final static String version = "1.7.2";
-    public final static String stage = "beta";
+    public final static String stage = "Release";
     public static String installation = "Minecade Mod";
 
     public static void main(String[] args) {
@@ -146,16 +143,6 @@ public class ModInstaller {
         }
     }
 
-    private static void addToList(File directory, List<File> files) {
-        for (File file : directory.listFiles()) {
-            if (file.isDirectory()) {
-                addToList(file, files);
-            } else {
-                files.add(file);
-            }
-        }
-    }
-
     public static String getOS() {
         if (isMac()) {
             return "Mac";
@@ -204,63 +191,6 @@ public class ModInstaller {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public static void addFilesToExistingZip(File zipFile, File[] files) throws IOException {
-        // get a temp file
-        File tempFile = File.createTempFile(zipFile.getName(), null);
-        // delete it, otherwise you cannot rename your existing zip to it.
-        tempFile.delete();
-
-        boolean renameOk = zipFile.renameTo(tempFile);
-        if (!renameOk) {
-            throw new RuntimeException("could not rename the file " + zipFile.getAbsolutePath() + " to " + tempFile.getAbsolutePath());
-        }
-        byte[] buf = new byte[1024];
-
-        ZipInputStream zin = new ZipInputStream(new FileInputStream(tempFile));
-        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFile));
-
-        ZipEntry entry = zin.getNextEntry();
-        while (entry != null) {
-            String name = entry.getName();
-            boolean notInFiles = true;
-            for (File f : files) {
-                if (f.getName().equals(name)) {
-                    notInFiles = false;
-                    break;
-                }
-            }
-            if (notInFiles) {
-                // Add ZIP entry to output stream.
-                out.putNextEntry(new ZipEntry(name));
-                // Transfer bytes from the ZIP file to the output file
-                int len;
-                while ((len = zin.read(buf)) > 0) {
-                    out.write(buf, 0, len);
-                }
-            }
-            entry = zin.getNextEntry();
-        }
-        // Close the streams
-        zin.close();
-        // Compress the files
-        for (int i = 0; i < files.length; i++) {
-            InputStream in = new FileInputStream(files[i]);
-            // Add ZIP entry to output stream.
-            out.putNextEntry(new ZipEntry(files[i].getName()));
-            // Transfer bytes from the file to the ZIP file
-            int len;
-            while ((len = in.read(buf)) > 0) {
-                out.write(buf, 0, len);
-            }
-            // Complete the entry
-            out.closeEntry();
-            in.close();
-        }
-        // Complete the ZIP file
-        out.close();
-        tempFile.delete();
     }
 
     public static boolean isWindows() {
